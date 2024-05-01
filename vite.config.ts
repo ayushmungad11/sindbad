4241ffeca112555b29edcf4a899af09c30019a97
Ayush <ayush.itweb@gmail.com>
2024-05-01 16:34:08 +0530

    done changes

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path'
+import { defineConfig } from "vite";
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+
+export default defineConfig({
+    plugins:[reactRefresh()],
+    build :{
+        emptyOutDir :true,
+        rollupOptions : {
+            input:{
+                main : resolve(__dirname,'index.html'),
+                about : resolve(__dirname, 'pages/about.html'),
+            }
+        }
+    }
+})
