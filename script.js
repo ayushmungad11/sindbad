229004c815f414964af05c13d021b0382f30d888
Ayush <ayush.itweb@gmail.com>
2024-05-20 18:56:01 +0530

    done changes

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        phone : document.getElementById("phone").value,
+        message : document.getElementById("message").value,
+
+    }
+    emailjs.send("service_1hzvyyq","template_ctco35r",parms).then(alert("Email Sent!!"))
+}
